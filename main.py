e5f94e0bfb7e2f1669fdccfa85e7f68ed999d3cc
Jonas Birkeli <jonas.birkeli@icloud.com>
2026-02-03 18:26:50 +0100

    TEST: Create simple test script for temp dataset

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from src.test import test_ir_plag
+
+
+def main():
+    # Program entrypoint
+    test_ir_plag()
+
+
+if __name__ == '__main__':
+    # We ballin
+    main()
+    sys.exit()
